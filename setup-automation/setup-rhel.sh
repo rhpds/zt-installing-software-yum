cb08395cbd68c03160c97a1eec62861983cbb1ff
Matt Micene (RHT) <mmicene@redhat.com>
2025-07-31 15:42:19 -0400

    Update to RHEL 10 and latest showroom

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
 dnf downgrade curl -y
+
 # modify pam files
 sed -i '/^#/{G;}' /etc/pam.d/password-auth
 sed -i '/^#/{G;}' /etc/pam.d/system-auth
-# install httpd
-dnf install -y httpd
 
-#set up tmux so it has to restart itself whenever the system reboots
-#step 1: make a script
-tee ~/startup-tmux.sh << EOF
-TMUX='' tmux new-session -d -s 'rhel-session' > /dev/null 2>&1
-tmux set -g pane-border-status top
-tmux setw -g pane-border-format ' #{pane_index} #{pane_current_command}'
-tmux set -g mouse on
-tmux set mouse on
-tmux unbind -n MouseDown3Pane
-EOF
+# install httpd moved to instances.yaml
+# dnf install -y httpd
 
-#step 2: make it executable
-chmod +x ~/startup-tmux.sh
-#step 3: use cron to execute 
-echo "@reboot ~/startup-tmux.sh" | crontab -
-
-#step 4: start tmux for the lab
-~/startup-tmux.sh
